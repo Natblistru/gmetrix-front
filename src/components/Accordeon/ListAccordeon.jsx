01ad6eda4b22614d87d83cd886cf6915f5069f1d
Bernavschi Natalia <natb@list.ru>
2023-05-29 12:25:49 +0300

    Completare cu continut static tema 1 istoria

--- src/components/Accordeon/ListAccordeon.jsx
+++ src/components/Accordeon/ListAccordeon.jsx
@@ -4,21 +4,25 @@ import ItemText from "./ItemText";
 
 const ListAccordeon = (props) => {
 const classes = " " + props.className;
+let titleList = props.teme[0].subtitles[0].subjects;
+let repereList = props.teme[0].subtitles[0].repere;
+let aplicatiiList = props.teme[0].subtitles[0].aplicatii;
+let termeniList = props.teme[0].subtitles[0].termeni;
 return <div className={classes}>
             <ItemAccordeon titlu="Teorie" {...props}>
-               <ItemList {...props} list={props.teme[0].subtitles[0].subjects}/>
+               <ItemList {...props} list={titleList}/>
             </ItemAccordeon>
             <ItemAccordeon titlu="Schemă recapitulativă" {...props}>
                <ItemText {...props}/>
             </ItemAccordeon>
             <ItemAccordeon titlu="Repere cronoligice" {...props}>
-               <ItemText {...props}/>
+            <ItemList {...props} list={repereList}/>
             </ItemAccordeon>
             <ItemAccordeon titlu="Termeni-cheie" {...props}>
-               <ItemText {...props}/>
+            <ItemList {...props} list={termeniList}/>
             </ItemAccordeon>
             <ItemAccordeon titlu="Aplicații (teste de examen)" {...props}>
-               <ItemText {...props}/>
+            <ItemList {...props} list={aplicatiiList}/>
             </ItemAccordeon>
        </div>;
 };
--- src/components/Accordeon/ListSubAccordeon.jsx
+++ src/components/Accordeon/ListSubAccordeon.jsx
@@ -3,25 +3,26 @@ import ItemList from "./ItemList";
 import ItemText from "./ItemText";
 
 const ListSubAccordeon = (props) => {
-const classes = " " + props.className;
-let arraySubject = props.teme[0].subtitles[0].subjects[0].vomAfla;
-return <div className={classes}>
-            <ItemAccordeon titlu="La aceasta lectie vom afla:" {...props}>
-               <ItemList {...props} list={arraySubject}/>
-            </ItemAccordeon>
-            <ItemAccordeon titlu="Studiaza interactiv" {...props}>
-               <ItemText {...props}/>
-            </ItemAccordeon>
-            {
-               arraySubject?.map(subj => (
-                  <ItemAccordeon titlu={subj.name} {...props} key={subj.id}>
-                     <ItemText {...props}/>
-                  </ItemAccordeon>
-               ))
-            }
-            <ItemAccordeon titlu="Evaluare (teste)" {...props}>
-               <ItemText {...props}/>
-            </ItemAccordeon>
-       </div>;
+  const classes = " " + props.className;
+  let arraySubject = props.teme[0].subtitles[0].subjects[0].vomAfla;
+  let arrayTests = props.teme[0].subtitles[0].subjects[0].teste;
+  return (
+    <div className={classes}>
+      <ItemAccordeon titlu="La aceasta lectie vom afla:" {...props}>
+        <ItemList {...props} list={arraySubject} />
+      </ItemAccordeon>
+      <ItemAccordeon titlu="Studiaza interactiv" {...props}>
+        <ItemText {...props} />
+      </ItemAccordeon>
+      {arraySubject?.map((subj) => (
+        <ItemAccordeon titlu={subj.name} {...props} key={subj.id}>
+          <ItemText {...props}>{subj.raspuns}</ItemText>
+        </ItemAccordeon>
+      ))}
+      <ItemAccordeon titlu="Evaluare (teste)" {...props}>
+      <ItemList {...props} list={arrayTests} />
+      </ItemAccordeon>
+    </div>
+  );
 };
 export default ListSubAccordeon;
