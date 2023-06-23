ecc6f50a5c5b49486df1a269765447c06a92c2b8
Bernavschi Natalia <natb@list.ru>
2023-06-23 17:22:11 +0300

    Add header la TestGeneralizator

--- src/components/Teste/TestGeneralizator.jsx
+++ src/components/Teste/TestGeneralizator.jsx
@@ -1,9 +1,58 @@
 import React, { useState } from "react";
-import temeIstoriArray from '../../data/temeIstoria';
+import temeIstoriArray from "../../data/temeIstoria";
 
 import ItemAccordeon from "../Accordeon/ItemAccordeon";
 import ItemText from "../Accordeon/ItemText";
 import ItemList from "../Accordeon/ItemList";
+
+const HeaderInit = () => {
+  return (
+    <div>
+      <div style={{ textAlign: "right", marginRight: "5px" }}>
+        Timp recomandat: 00:05:00
+      </div>
+    </div>
+  );
+};
+
+const Header = ({ activeButton, handleClick }) => {
+  return (
+    <div className="nav-header">
+      <div className="nav-header">
+        <button
+          className="circle"
+          style={{ backgroundColor: activeButton == 1 ? "#76a900" : "#fff" }}
+          onClick={() => handleClick(1)}
+        >
+          1
+        </button>
+        <button
+          className="circle"
+          style={{ backgroundColor: activeButton == 2 ? "#76a900" : "#fff" }}
+          onClick={() => handleClick(2)}
+        >
+          2
+        </button>
+        <button
+          className="circle"
+          style={{ backgroundColor: activeButton == 3 ? "#76a900" : "#fff" }}
+          onClick={() => handleClick(3)}
+        >
+          3
+        </button>
+        <button
+          className="circle"
+          style={{ backgroundColor: activeButton == 4 ? "#76a900" : "#fff" }}
+          onClick={() => handleClick(4)}
+        >
+          4
+        </button>
+        <a onClick={() => handleClick(null)}>Lista de sarcini</a>
+      </div>
+      <div>Timp recomandat: 00:05:00</div>
+    </div>
+  );
+};
 
 // let list = temeIstoriArray[0].subtitles[0].subjects[0].teste[9];
 let currentIndex = 0;
@@ -16,6 +65,12 @@ const TestGeneralizator = ({
   handleTryAgain,
 }) => {
   const [selectedValues, setSelectedValues] = useState([]);
+  // const [showHeader, setShowHeader] = useState(false);
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleClick = (buttonId) => {
+    setActiveButton(buttonId);
+  };
 
   // const [correctAnswer, setCorrectAnswer] = useState(null);
 
@@ -35,18 +90,38 @@ const TestGeneralizator = ({
       .filter((answer) => answer.correct)
       .map((answer) => answer.text);
     const selectedValuesString = selectedValues.sort().join(",");
-    const correctValuesString = correctValues.sort().join(","); 
+    const correctValuesString = correctValues.sort().join(",");
     setCorrectAnswer(selectedValuesString === correctValuesString);
+    // setShowHeader(true);
+    setActiveButton(1);
   };
   const handleTryAgainClearCheck = () => {
-    setSelectedValues([]); 
+    setSelectedValues([]);
     handleTryAgain();
   };
 
   return (
     <>
+      {!activeButton && <HeaderInit />}
+      {console.log(activeButton)}
+      {activeButton && (
+        <Header activeButton={activeButton} handleClick={handleClick} />
+      )}
       <ItemAccordeon titlu="Lista de sarcini" open={true}>
-      <ItemList list={list.quizArray[currentIndex].listaSarcini} />
+        <div className="subjects-container ">
+          {list.quizArray[currentIndex].listaSarcini?.map((subtitle, idx) => {
+            return (
+              <div key={idx} className="subject-item">
+                <div className="title-item">
+                  <div className="num-item">{subtitle.id}.</div>
+                  <div className="name-item" onClick={() => handleClick(subtitle.id)}>{subtitle.name}</div>
+                </div>
+                <div className="points">V</div>
+              </div>
+            );
+          })}
+        </div>
+
         {correctAnswer === null && (
           <button onClick={checkAnswer} className="btn-test">
             Incepe testul
@@ -55,7 +130,9 @@ const TestGeneralizator = ({
       </ItemAccordeon>
       {correctAnswer !== null && (
         <ItemAccordeon
-          titlu={`Rezolvarea sarcinii (${currentIndex + 1}/${list.quizArray.length}):`}
+          titlu={`Rezolvarea sarcinii (${currentIndex + 1}/${
+            list.quizArray.length
+          }):`}
           open={true}
         >
           <ItemText classNameChild="">
