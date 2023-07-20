acdf174e4073332de7dea0f03e74d763df337f2c
Bernavschi Natalia <natb@list.ru>
2023-07-20 13:07:09 +0300

    Afisare progres la subtitles pe pag temelor

--- src/components/SubTopicItem.jsx
+++ src/components/SubTopicItem.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import temeIstoriArray from "../data/temeIstoria";
 import ProgressBar from "./ProgressBar";
 
-const SubTopicItem = (props) => {
-  const subtitle = props.subTit;
+const SubTopicItem = ({subTit,idx,results}) => {
+  const subtitle = subTit;
+  const [procent, setProcent] = useState(0);
+
+  useEffect(() => {
+    console.log(results.items);
+    if (subtitle.id !== null && subtitle.id !== undefined) {
+      const filteredIds = temeIstoriArray
+        .flatMap((item) =>
+          item.subtitles.flatMap((subItem) => subItem.subjects)
+        )
+        .filter((subjectItem) => subjectItem.subtitleID == subtitle.id)
+        .map((subjectItem) => subjectItem.id);
+      
+      let SumProcValue = 0;
+
+      filteredIds.forEach((id) => {
+        const foundObject = results.items.find((item) => {
+          return item.subject.find((subItem) => subItem.id === id);
+        });
+
+        let procValue = null;
+
+        if (foundObject) {
+          procValue = foundObject.subject.find(
+            (subItem) => subItem.id === id
+          )?.proc;
+        }
+        if (procValue !== null && procValue !== undefined)
+        SumProcValue+=procValue;
+      });
+      SumProcValue = SumProcValue/filteredIds.length;
+      setProcent(SumProcValue);
+    }
+  }, [results.items]);
+
   return (
-    <li key={props.idx}>
+    <li key={idx}>
       <div className="subtopic-header">
         <span className="num"></span>
         <h4>
@@ -15,8 +51,11 @@ const SubTopicItem = (props) => {
           </Link>
         </h4>
       </div>
-      <ProgressBar proc={20} nota={10}/>
+      <ProgressBar proc={procent} nota={10}/>
     </li>
   );
 };
-export default SubTopicItem;
+const reduxState = (state) => ({
+  results: state.results,
+});
+export default connect(reduxState, null)(SubTopicItem);
