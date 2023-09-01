0f4e04e0a19178ca280a65cb290fe1487e68959c
Bernavschi Natalia <natb@list.ru>
2023-09-02 00:29:34 +0300

    Add lista teme matematica

---
+++ src/data/temeMatem.jsx
@@ -0,0 +1,148 @@
+let temeMatem = [
+  {
+    id: "1",
+    title: "Mulţimi numerice.",
+    disciplina: "Matematica",
+    subtitles: [
+      {
+        id: "1",
+        name: "Mulțimea numerelor naturale",
+      },
+      {
+        id: "2",
+        name: "Mulțimea numerelor întregi",
+      },
+      {
+        id: "3",
+        name: "Mulțimea numerelor raționale",
+      }, 
+      {
+        id: "4",
+        name: "Mulțimea numerelor reale",
+      },              
+    ]
+  },
+  {
+    id: "2",
+    title: "Rapoarte şi proporţii.",
+    disciplina: "Matematica",
+    subtitles: [
+      {
+        id: "1",
+        name: "Rapoarte. Proporții.",
+      },
+      {
+        id: "2",
+        name: "Procente. Media aritmetică.",
+      },           
+    ]
+  },
+  {
+    id: "3",
+    title: "Calcul algebric.",
+    disciplina: "Matematica",
+    subtitles: [
+      {
+        id: "1",
+        name: "Expresii algebrice.",
+      },
+      {
+        id: "2",
+        name: "Rapoarte algebrice.",
+      },      
+    ]
+  },
+  {
+    id: "3",
+    title: "Calcul algebric. Polinoame. Fracţii algebrice.",
+    disciplina: "Matematica",
+    subtitles: [
+      {
+        id: "1",
+        name: "Expresii algebrice.",
+      },
+    ]
+  },
+  {
+    id: "4",
+    title: "Funcţii.",
+    disciplina: "Matematica",
+    subtitles: [
+      {
+        id: "1",
+        name: "Noţiune de funcţie.",
+      },
+      {
+        id: "2",
+        name: "Funcţia de gradul I.",
+      },  
+      {
+        id: "3",
+        name: "Funcţia de gradul II.",
+      },               
+    ]
+  },
+  {
+    id: "5",
+    title: "Ecuaţii, inecuaţii, sisteme de ecuaţii, sisteme de inecuaţii.",
+    disciplina: "Matematica",
+    subtitles: [
+      {
+        id: "1",
+        name: "Ecuaţii, inecuaţii, sisteme de inecuaţii de gradul I cu o necunoscută.",
+      },
+      {
+        id: "2",
+        name: "Ecuaţii, sisteme de ecuaţii de gradul I cu două necunoscute.",
+      },  
+      {
+        id: "3",
+        name: "Ecuaţii și inecuaţii de gradul II cu o necunoscută.",
+      },   
+      {
+        id: "4",
+        name: "Ecuaţii şi sisteme de ecuaţii raţionale cu o necunoscută.",
+      },                    
+    ]    
+  },
+  {
+    id: "6",
+    title: "Geometrie.",
+    disciplina: "Matematica",
+    subtitles: [
+      {
+        id: "1",
+        name: "Unități de măsură.",
+      },
+      {
+        id: "2",
+        name: "Noțiuni geometrice fundamentale.",
+      },
+      {
+        id: "3",
+        name: "Triunghiuri.",
+      },
+      {
+        id: "4",
+        name: "Patrulatere. Poligoane.",
+      },
+      {
+        id: "5",
+        name: "Cercul. Discul.",
+      },
+      {
+        id: "5",
+        name: "Arii.",
+      },
+      {
+        id: "6",
+        name: "Poliedre.",
+      },
+      {
+        id: "7",
+        name: "Corpuri de rotație.",
+      },
+    ]
+  },
+];
+export default temeMatem;
--- src/pages/TemeMatem.js
+++ src/pages/TemeMatem.js
@@ -1,4 +1,22 @@
+import temeMatem from '../data/temeMatem';
+import Breadcrumb from "../components/Breadcrumb";
+import Titlu from "../components/Titlu";
+import Wrapper from "../components/Wrapper";
+import TitleBox from '../components/TitleBox';
+import TopicsList from '../components/TopicsList';
+import Card from '../components/Card';
+import '../index.css';
+
 const TemeMatem = () => {
-    return <h2>TemeMatem</h2>
+    return (
+        <Wrapper className="large">
+            <Breadcrumb list={[{name: "Discipline", path: "/"}]}/>
+            <Card>
+                <Titlu className="titlu-card">Matematica - pregătire pentru examen clasa 9</Titlu>
+                <TitleBox className="teme-container">clasa 9</TitleBox>
+                <TopicsList teme={temeMatem}/>
+            </Card>
+        </Wrapper>
+    )
 }
 export default TemeMatem;
