993cec8d2885e164079b3c550b8c41437eb90378
Bernavschi Natalia <natb@list.ru>
2023-11-19 09:26:05 +0200

    Add axios test Home, TopicList.

--- src/components/TopicsList.jsx
+++ src/components/TopicsList.jsx
@@ -1,6 +1,20 @@
+import axios from "axios";
+import {useEffect} from "react";
 import TopicItem from "./TopicItem";
 
 const TopicsList = (props) => {
+    useEffect(()=> {
+        fetchTodos();
+    },[]);
+
+    const fetchTodos = async () => {
+        try {
+            const res = await axios.get("http://localhost:8000/api/chapters?subject_study_level_id=1");
+            console.log(res.data);
+        } catch (err) {
+            console.error(err);
+        }
+    }
     return (                
     <ol className="topic-list" type="I">
         {props.teme.map((tema,idx) => (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
+import axios from "axios";
+import {useState, useEffect} from "react";
+
 import ListDiscipline from "../components/ListDiscipline";
 import Titlu from "../components/Titlu";
 import Wrapper from "../components/Wrapper";
 import discipline from "../data/discipline";
 
 const Home = () => {
+  const [chapters,setChapters] = useState([]);
+  useEffect(()=> {
+    fetchChapters();
+  },[]);
+  const fetchChapters = async () => {
+    try {
+      const res = await axios.get("http://localhost:8000/api/subject_study_levels");
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
     <Wrapper>
       <Titlu>Disciplinele de absolvire a cursului gimnazial</Titlu>
