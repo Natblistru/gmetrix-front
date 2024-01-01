c4dce1e1f48800698d6fd8a1ef6e6788988a16bf
Bernavschi Natalia <natb@list.ru>
2024-01-01 11:01:48 +0200

    Add filters for ViewSummativeTestItems in dashboard

--- src/components/admin/Test/ViewSummativeTestItem.js
+++ src/components/admin/Test/ViewSummativeTestItem.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { debounce } from "lodash"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
 import Paginator from '../Paginator';
 
 const SORT_ASC = "asc"
 const SORT_DESC = "desc"
+const PER_PAGE_OPTIONS = [5,10,20,50,100]
 
 const TableHeader = ({ columns, handleSort, sortColumn, sortOrder}) => {
   return (
@@ -103,6 +106,10 @@ function ViewSummativeTestItem() {
   const [perPage, setPerPage] = useState(10)
   const [pagination, setPagination] = useState({})
   const [currentPage, setCurrentPage] = useState(1)
+  const [blockFilterVisible, setBlockFilterVisible] = useState(false);
+  const [learningProgramList, setLearningProgramList] = useState([]);
+  const [themeList, setThemeList] = useState([]);
+  const [teacherList, setTeacherList] = useState([]);
 
   const handleSort = (column) => {
     if (column === sortColumn) {
@@ -127,6 +134,57 @@ function ViewSummativeTestItem() {
     setPerPage(perPage)
   }
 
+  const toggleblockVisible = () => {
+    setBlockFilterVisible(!blockFilterVisible);
+  };
+
+  const [filter, setFilter] = useState({
+    learning_program_id: '',
+    theme_learning_program_id: '',
+    teacher_id: '',
+  })
+
+  const handleInput = (e) => {
+    e.persist();
+    const { name, value } = e.target;
+  
+    setFilter((prevFilter) => {
+      const updatedFilter = { ...prevFilter };
+  
+      if (name === 'learning_program_id') {
+        updatedFilter[name] = value;
+        updatedFilter.theme_learning_program_id = '';
+      } else {
+        updatedFilter[name] = value;
+      }
+  
+      return updatedFilter;
+    });
+  };
+  
+
+  useEffect(() => {
+
+    axios.get('http://localhost:8000/api/all-learningPrograms').then(res=>{
+      if(res.data.status === 200){
+        setLearningProgramList(res.data.learningProgram);
+      }
+    });
+
+    axios.get('http://localhost:8000/api/all-themeLearningPrograms').then(res=>{
+      if(res.data.status === 200){
+        setThemeList(res.data.theme);
+      }
+    });
+
+    axios.get('http://localhost:8000/api/all-teachers').then(res=>{
+      if(res.data.status === 200){
+        setTeacherList(res.data.teachers);
+      }
+    });
+
+  },[])
+
   useEffect(()=>{
     const fetchData = async () => {
       try {
@@ -136,6 +194,9 @@ function ViewSummativeTestItem() {
           sortOrder: sortOrder,
           perPage: perPage,
           page: currentPage,
+          filterProgram: filter.learning_program_id,
+          filterTheme: filter.theme_learning_program_id,
+          filterTeacher: filter.teacher_id,
         };
         const response = await axios.get('http://localhost:8000/api/view-summative-test-item', { params });
           if (response.data.status === 200) {
@@ -150,7 +211,7 @@ function ViewSummativeTestItem() {
       }
     }
     fetchData();
-  }, [sortColumn, sortOrder, search, perPage, currentPage]);
+  }, [sortColumn, sortOrder, search, perPage, currentPage, filter ]);
 
   const commonColumns = {
     'editLink': (item) => (
@@ -183,19 +244,72 @@ function ViewSummativeTestItem() {
               <div className="input-group">
                   <label className="mt-2 me-2">Per page</label>
                   <select className="form-select" value={perPage} onChange={(e) => handlePerPage(e.target.value)}>
-                      <option value="5">5</option>
-                      <option value="10">10</option>
-                      <option value="20">20</option>
-                      <option value="50">50</option>
+                      {PER_PAGE_OPTIONS.map((perPage) => {
+                        return (
+                          <option key={perPage}>{perPage}</option>
+                        )
+                      })}
+
                   </select>
               </div>
           </div>
-          <div className="col-md-7">
+          <div className="col-md-4">
+
+          </div>
+          <div className="col-md-3">
+          <FontAwesomeIcon icon={faFilter} onClick={toggleblockVisible} className="btnBts btn-outline-secondary mt-1"/>
           <Link to="/admin/add-summative-test-item" className="btnBts btn-primary text-white float-end">Add Summative Test Items</Link>
           </div>
         </div>
+        {blockFilterVisible && 
+          <div className="rowBts m-2">
+            <div className="col-md-4">
+                <div className="form-group">
+                  <select name="learning_program_id" onChange={handleInput} value={filter.learning_program_id} className="form-control">  
+                    <option value="">Select program</option>
+                    {
+                      learningProgramList.map((item)=> {
+                        return (
+                          <option value={item.id} key={item.id}>{item.name}</option>
+                        )
+                      })
+                    }
+                  </select>            
+                </div>
+            </div>
+            <div className="col-md-4">          
+                <div className="form-group">
+                  <select name="theme_learning_program_id" onChange={handleInput} value={filter.theme_learning_program_id} className="form-control">  
+                    <option option value="">Select Theme</option>
+                    {themeList
+                      .filter((item) => item.learning_program_id == filter.learning_program_id)
+                      .map((item) => (
+                        <option value={item.id} key={item.id}>
+                          {item.name}
+                        </option>
+                    ))}
+                  </select>            
+                </div>
+              </div>
+              <div className="col-md-4">
+                <div className="form-group">
+                  <select name="teacher_id" onChange={handleInput} value={filter.teacher_id} className="form-control">  
+                    <option option value="">Select Teacher</option>
+                    {
+                      teacherList.map((item)=> {
+                        return (
+                          <option value={item.id} key={item.id}>{item.name}</option>
+                        )
+                      })
+                    }
+                  </select>            
+                </div>
+              </div> 
+
+          </div>
+        }
         <div className="card-body">
-          <table className="table table-primary table-bordered table-responsive table-striped">
+        <table className={`table table-primary table-bordered table-responsive table-striped ${teacherTopicList.length == 0 ? 'table-fixed' : ''}`}>
             <TableHeader
               columns={columns_header}
               handleSort={handleSort}
