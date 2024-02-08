91a8ca39f378b4bc79b0d9f2b695c7544246a89c
Bernavschi Natalia <natb@list.ru>
2024-02-08 17:54:55 +0200

    Save updated breadcrumb to LocaleStorage

--- src/components/ReduxComp/breadcrumbReducer.js
+++ src/components/ReduxComp/breadcrumbReducer.js
@@ -1,12 +1,14 @@
-import { ADD_BREADCRUMB } from './actions';
-import { UPDATE_SUBJECT_BREADCRUMB } from './actions';
-import { UPDATE_TOPIC_BREADCRUMB } from './actions';
-import { UPDATE_SUBTOPIC_BREADCRUMB } from './actions';
+import { ADD_BREADCRUMB, UPDATE_SUBJECT_BREADCRUMB, UPDATE_TOPIC_BREADCRUMB, UPDATE_SUBTOPIC_BREADCRUMB } from './actions';
+
+// Funcție pentru a actualiza și salva în LocalStorage
+const updateAndSaveToLocalStorage = (state) => {
+  localStorage.setItem('breadcrumbs', JSON.stringify(state));
+  return state;
+};
 
 const initialState = JSON.parse(localStorage.getItem('breadcrumbs')) || [];
 
 const breadcrumbReducer = (state = initialState, action) => {
-
   switch (action.type) {
     case ADD_BREADCRUMB:
       const exists = state.some(item => (
@@ -16,37 +18,36 @@ const breadcrumbReducer = (state = initialState, action) => {
         return state;
       } else {
         const newState = [...state, action.payload];
-        localStorage.setItem('breadcrumbs', JSON.stringify(newState));
-        return newState;
+        return updateAndSaveToLocalStorage(newState);
       }
 
     case UPDATE_SUBJECT_BREADCRUMB:
       if (state.length === 1) {
-        return [...state, action.payload];
+        return updateAndSaveToLocalStorage([...state, action.payload]);
       } else if (state.length > 1) {
         const updatedBreadcrumb = [...state];
         updatedBreadcrumb[1] = action.payload;
-        return updatedBreadcrumb;
+        return updateAndSaveToLocalStorage(updatedBreadcrumb);
       }
       return state;
 
     case UPDATE_TOPIC_BREADCRUMB:
       if (state.length === 2) {
-        return [...state, action.payload];
+        return updateAndSaveToLocalStorage([...state, action.payload]);
       } else if (state.length > 2) {
         const updatedBreadcrumb = [...state];
         updatedBreadcrumb[2] = action.payload;
-        return updatedBreadcrumb;
+        return updateAndSaveToLocalStorage(updatedBreadcrumb);
       }
       return state;
 
     case UPDATE_SUBTOPIC_BREADCRUMB:
       if (state.length === 3) {
-        return [...state, action.payload];
+        return updateAndSaveToLocalStorage([...state, action.payload]);
       } else if (state.length > 3) {
         const updatedBreadcrumb = [...state];
         updatedBreadcrumb[3] = action.payload;
-        return updatedBreadcrumb;
+        return updateAndSaveToLocalStorage(updatedBreadcrumb);
       }
       return state;
 
